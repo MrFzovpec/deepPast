a52a938d015076d77537f76e683966e53c419318
Tikhon <41113186+TikhonP@users.noreply.github.com>
2019-11-30 22:41:58 +0300

    Нормальный лендинг на flask
    Петя простоно захостить не может, вот и приходится делать все за него!

---
+++ frontend/Нормальный лендинг на flask/serve.py
@@ -0,0 +1,20 @@
+# serve.py
+
+from flask import Flask, render_template, url_for
+
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/documenation")
+def two():
+    return render_template('docs.html')
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True, port=80)
